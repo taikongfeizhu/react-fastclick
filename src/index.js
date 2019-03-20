4e53f7188ef83d3375649662b0d600436b0a46e5
huangjian <huangjian@bytedance.com>
2019-03-20 14:37:04 +0800

    feat(index.js):add ignore-class
    Change-Id: I92db03e04d97aaa53dc9145abea589b9e644988d

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@
       // Moved if Math.abs(downX - upX) > MOVE_THRESHOLD;
       var MOVE_THRESHOLD = 8;
       var TOUCH_DELAY = 1000;
+      var ignoreClass = 'needsclick';
 
       var touchKeysToStore = [
         'clientX',
@@ -232,7 +233,10 @@
           (props && typeof props.onClick === 'function') || handleType[type]
         )) {
           // Add our own events to props
-          args[1] = propsWithFastclickEvents(type, props || {});
+          var classes = new Set(props.className && props.className.split(" "));
+          if (!classes.has(ignoreClass)) {
+            args[1] = propsWithFastclickEvents(type, props || {});
+          }
         }
 
         // Apply args to original createElement function
